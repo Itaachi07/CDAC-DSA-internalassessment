0c7f158246f6eb7c27461e53f03ea0a497fa176d
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-05-03 18:58:21 +0530

    Linked List creation started

--- Vehicle/src/LinkedList.java
+++ Vehicle/src/LinkedList.java
@@ -1,4 +1,34 @@
 
-public class LinkedList {
-
+public class LinkedList 
+{
+	static class Node
+	{
+		private int data;
+		private Node prev;
+		private Node next;
+		
+		Node(int data)
+		{
+			this.data=data;
+			this.prev=null;
+			this.next=null;
+		}
+	}
+	private Node head;
+	private int nodesCount;
+	
+	LinkedList()
+	{
+		head = null;
+		nodesCount = 0;
+	}
+	boolean isListEmpty()
+	{
+		return(head == null);
+	}
+	int getNodesCount()
+	{
+		return(this.nodesCount);
+	}
+	
 }
